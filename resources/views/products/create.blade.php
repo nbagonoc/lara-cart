b8fec2372360deffde7d6041a00a5f88ccde7929
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-07-22 14:56:47 +0800

    improved forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,30 +10,33 @@
                 <div class="card-header">Create Product</div>
                 <div class="card-body">
                     {!! Form::open(['action' => 'ProductController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                        <div class="form-group">
-                            {{Form::label('name', 'Name')}}
-                            {{Form::text('name', '', ['class'=>'form-control', 'placeholder'=>'Enter the product name'])}}
+                        <div class="form-row">
+                            <div class="form-group col-md-8">
+                                {{Form::label('name', 'Name')}}
+                                {{Form::text('name', '', ['class'=>'form-control', 'placeholder'=>'Product name'])}}
+                            </div>
+                            <div class="form-group col-md-4">
+                                {{Form::label('price', 'Price')}}
+                                {{Form::number('price', '', ['class'=>'form-control','placeholder'=>'Product price'])}}
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                {{Form::label('category', 'Category')}}
+                                {{Form::select('category', ['phones' => 'Phones', 'tablets' => 'Tablets', 'computers' => 'Computers', 'cameras' => 'Cameras', 'others' => 'Others'],null,['class'=>'form-control', 'placeholder'=>'Select a category'])}}
+                            </div>
+                            <div class="form-group col-md-6">
+                                {{Form::label('status', 'Status')}}
+                                {{Form::select('status', ['available' => 'Available', 'out-of-stock' => 'Out of Stock'],null,['class'=>'form-control'])}}
+                            </div>
                         </div>
                         <div class="form-group">
-                            {{Form::label('price', 'Price')}}
-                            {{Form::number('price', '', ['class'=>'form-control','placeholder'=>'Enter the price of the product'])}}
-                        </div>
-                        <div class="form-group">
-                            {{Form::label('category', 'Category')}}
-                            {{Form::select('category', ['phones' => 'Phones', 'tablets' => 'Tablets', 'computers' => 'Computers', 'cameras' => 'Cameras', 'others' => 'Others'],null,['class'=>'form-control', 'placeholder'=>'Select a category'])}}
-                        </div>
-                        <div class="form-group">
-                            {{Form::label('status', 'Status')}}
-                            {{Form::select('status', ['available' => 'Available', 'out-of-stock' => 'Out of Stock'],null,['class'=>'form-control'])}}
+                            {{Form::label('imgPath', 'Featured Image')}}
+                            {{Form::file('imgPath',['class'=>'form-control'])}}
                         </div>
                         <div class="form-group">
                             {{Form::label('description', 'Description')}}
-                            {{Form::textarea('description', '', ['id'=>'article-ckeditor','class'=>'form-control', 'placeholder'=>'Enter product description'])}}
-                        </div>
-                        <div class="form-group">
-                            <div class="custom-file">
-                                {{Form::file('imgPath')}}
-                            </div>
+                            {{Form::textarea('description', '', ['id'=>'article-ckeditor','class'=>'form-control', 'placeholder'=>'Product description'])}}
                         </div>
                         {{Form::submit('Submit', ['class'=>'btn btn-outline-success btn-sm'])}}
                         <a href="{{ URL::previous() }}" class="btn btn-outline-danger btn-sm">Cancel</a>
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -10,28 +10,33 @@
                 <div class="card-header">Update Product</div>
                 <div class="card-body">
                     {!! Form::open(['action' => ['ProductController@update', $product->id],  'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                        <div class="form-group">
-                            {{Form::label('name', 'Name')}}
-                            {{Form::text('name', $product->name, ['class'=>'form-control', 'placeholder'=>'Enter the product name'])}}
+                        <div class="form-row">
+                            <div class="form-group col-md-8">
+                                {{Form::label('name', 'Name')}}
+                                {{Form::text('name', $product->name, ['class'=>'form-control', 'placeholder'=>'Product name'])}}
+                            </div>
+                            <div class="form-group col-md-4">
+                                {{Form::label('price', 'Price')}}
+                                {{Form::number('price', $product->price, ['class'=>'form-control','placeholder'=>'Product price'])}}
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                {{Form::label('category', 'Category')}}
+                                {{Form::select('category', ['phones' => 'Phones', 'tablets' => 'Tablets', 'computers' => 'Computers', 'cameras' => 'Cameras', 'others' => 'Others'], $product->category,['class'=>'form-control', 'placeholder'=>'Select a category'])}}
+                            </div>
+                            <div class="form-group col-md-6">
+                                {{Form::label('status', 'Status')}}
+                                {{Form::select('status', ['available' => 'Available', 'out-of-stock' => 'Out of Stock'], $product->status,['class'=>'form-control'])}}
+                            </div>
                         </div>
                         <div class="form-group">
-                            {{Form::label('price', 'Price')}}
-                            {{Form::number('price', $product->price, ['class'=>'form-control','placeholder'=>'Enter the price of the product'])}}
-                        </div>
-                        <div class="form-group">
-                            {{Form::label('category', 'Category')}}
-                            {{Form::select('category', ['phones' => 'Phones', 'tablets' => 'Tablets', 'computers' => 'Computers', 'cameras' => 'Cameras', 'others' => 'Others'], $product->category,['class'=>'form-control', 'placeholder'=>'Select a category'])}}
-                        </div>
-                        <div class="form-group">
-                            {{Form::label('status', 'Status')}}
-                            {{Form::select('status', ['available' => 'Available', 'out-of-stock' => 'Out of Stock'], $product->status,['class'=>'form-control'])}}
+                            {{Form::label('imgPath', 'Featured image')}}
+                            {{Form::file('imgPath',['class'=>'form-control'])}}
                         </div>
                         <div class="form-group">
                             {{Form::label('description', 'Description')}}
-                            {{Form::textarea('description', $product->description, ['id'=>'article-ckeditor','class'=>'form-control', 'placeholder'=>'Enter product description'])}}
-                        </div>
-                        <div class="form-group">
-                            {{Form::file('imgPath')}}
+                            {{Form::textarea('description', $product->description, ['id'=>'article-ckeditor','class'=>'form-control', 'placeholder'=>'product description'])}}
                         </div>
                         {{Form::hidden('_method','PATCH')}}
                         {{Form::submit('Submit', ['class'=>'btn btn-outline-success btn-sm'])}}

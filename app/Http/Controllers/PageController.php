29058f26f276cd3f1bce5cd1b6055e96ac7a25ba
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-07-23 10:37:14 +0800

    changed asc to dsc for pagination

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,14 +9,14 @@ class PageController extends Controller
 {
     public function home(){
         // $products = Product::all();
-        $products = Product::orderBy('created_at','asc')->paginate(6);
+        $products = Product::orderBy('created_at','dsc')->paginate(6);
         return view('pages.home')->with('products',$products);
     }
 
     public function shopByCategory($category){
         // dd($products);
         // $products = Product::where('category',$category)->get();
-        $products = Product::where('category',$category)->orderBy('created_at','asc')->paginate(6);
+        $products = Product::where('category',$category)->orderBy('created_at','dsc')->paginate(6);
         return view('pages.shop')->with('products',$products);
     }
 }

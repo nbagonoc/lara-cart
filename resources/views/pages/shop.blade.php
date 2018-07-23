93c29f1848b6b652bba5818dd482bd4138305630
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-07-23 15:35:58 +0800

    improved UI

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -19,15 +19,15 @@
                                 <div class="card-body">
                                     <a href="/product/{{$product->id}}" class="text-dark"><h6 class="text-capitalize font-weight-light mb-0">{{$product->name}}</h6></a>
                                     <span class="text-capitalize text-secondary small">{{$product->category}}</span>
-                                    <h5 class="text-success">${{number_format((float)$product->price,2,'.','')}}</h5>
-                                    @guest
+                                    <h5 class="text-success mt-2 mb-0">${{number_format((float)$product->price,2,'.','')}}</h5>
+                                    {{-- @guest
                                         <a href="/add-to-cart/{{$product->id}}" class="btn btn-outline-success btn-block mt-2">Add to Cart</a>
                                     @endguest
                                     @auth
                                         @if(Auth::user()->role=='user')
                                             <a href="/add-to-cart/{{$product->id}}" class="btn btn-outline-success btn-block mt-2">Add to Cart</a>
                                         @endif
-                                    @endauth
+                                    @endauth --}}
                                 </div>
                             </div>
                         </div>
